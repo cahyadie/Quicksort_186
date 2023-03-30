b3417db6e15d669beed2f87dbed6dd5457ceb4e4
cahyadie <cahyadiilham12@gmail.com>
2023-03-30 15:51:26 +0700

    membuat display

--- Quicksort_186/Quicksort_186.cpp
+++ Quicksort_186/Quicksort_186.cpp
@@ -65,6 +65,7 @@ void q_short(int low, int high)
 			cmp_count++;
 		}
 		cmp_count++;
+
 		if (i < j)
 		{
 			swap(i, j);
@@ -78,4 +79,25 @@ void q_short(int low, int high)
 	}
 	q_short(low, j - 1);
 	q_short(j + 1, high);
+}
+
+void display() {
+	cout << "\n-----------=" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+}
+
+
+
+int main()
+{
+
 }
